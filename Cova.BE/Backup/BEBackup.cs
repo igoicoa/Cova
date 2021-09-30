0010c14baaf96f3efb153648ef09d6bb5980796c
igoicoa <igoicoa@hotmail.com>
2021-09-30 19:51:42 -0300

    add ignored files

---
+++ Cova.BE/Backup/BEBackup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Cova.BE.Backup
+{
+    public class BEBackup
+    {
+        public int BackupId { get; set; }
+        public BEUsuario Usuario { get; set; }
+        public DateTime FechaBackup { get; set; }
+        public string Archivo { get; set; }
+        public string Nombre { get; set; }
+    }
+}
---
+++ Cova.Servicios/Backup/Backup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Cova.BE.Backup;
+using Cova.MPP;
+
+namespace Cova.Servicios.Backup
+{
+    public static class Backup
+    {
+        public static bool RealizarBakup(BEBackup backup)
+        {
+            MPPBackup mPPBackup = new MPPBackup();
+            try
+            {
+                backup.Archivo = Directory.GetCurrentDirectory() + "\\" + backup.Nombre + ".bak";
+                return mPPBackup.RealizarBackup(backup);
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static bool RealizarRestore(BEBackup backup)
+        {
+            MPPBackup mPPBackup = new MPPBackup();
+            try
+            {
+                return mPPBackup.RealizarRestore(backup);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static IList<BEBackup> ObtenerBackups()
+        {
+            MPPBackup mPPBackup = new MPPBackup();
+            try
+            {
+                return mPPBackup.ObtenerBackups();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
